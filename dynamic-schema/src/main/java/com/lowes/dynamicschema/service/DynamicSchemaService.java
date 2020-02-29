9bc0cec9a41b9fcd2a86c4e2e8e97375280b87db
Naveen Devarajaiah <naveen.d088@gmail.com>
2020-02-29 13:37:36 +0530

    Update DynamicSchemaService.java

--- dynamic-schema/src/main/java/com/lowes/dynamicschema/service/DynamicSchemaService.java
+++ dynamic-schema/src/main/java/com/lowes/dynamicschema/service/DynamicSchemaService.java
@@ -1,7 +1,3 @@
-/****************************************************************
- * Copyright (C) Lowe's Companies, Inc. All rights reserved. This file is for internal use only at
- * Lowe's Companies, Inc.
- ****************************************************************/
 package com.lowes.dynamicschema.service;
 
 import java.io.IOException;
@@ -17,87 +13,67 @@ import org.springframework.util.FileCopyUtils;;
 @Service
 public class DynamicSchemaService {
 
-  @SuppressWarnings("unchecked")
-  public Object retrieveInfoPerRequest(String incomingRequest) throws IOException {
+	@SuppressWarnings("unchecked")
+	public Object retrieveInfoPerRequest(String incomingRequest) throws IOException {
 
-    ClassPathResource propsFile = new ClassPathResource("response.json");
-    byte[] bdata = FileCopyUtils.copyToByteArray(propsFile.getInputStream());
-    String actualJson = new String(bdata, StandardCharsets.UTF_8);
+		ClassPathResource propsFile = new ClassPathResource("response.json");
+		byte[] bdata = FileCopyUtils.copyToByteArray(propsFile.getInputStream());
+		String actualJson = new String(bdata, StandardCharsets.UTF_8);
 
-    JSONObject actualJsonObj = new JSONObject(actualJson);
+		JSONObject actualJsonObj = new JSONObject(actualJson);
 
-    JSONObject expectedJsonObj = new JSONObject(incomingRequest);
+		JSONObject expectedJsonObj = new JSONObject(incomingRequest);
 
-    JSONObject finalObj = new JSONObject();
+		Map<String, Object> actualJsonObjMap = actualJsonObj.toMap();
 
-    Map<String, Object> actualJsonObjMap = actualJsonObj.toMap();
+		Map<String, Object> expectedJsonObjMap = expectedJsonObj.toMap().containsKey("template")
+				? (Map<String, Object>) expectedJsonObj.toMap().get("template")
+				: expectedJsonObj.toMap();
 
-    Map<String, Object> expectedJsonObjMap = expectedJsonObj.toMap().containsKey("template")
-        ? (Map<String, Object>) expectedJsonObj.toMap().get("template")
-        : expectedJsonObj.toMap();
+		return retrieveNestedMapping((Map<String, Object>) expectedJsonObjMap, (Map<String, Object>) actualJsonObjMap);
 
-    expectedJsonObjMap.keySet().stream().forEach(key -> {
+	}
 
-      if (actualJsonObjMap.containsKey(key)) {
+	@SuppressWarnings("unchecked")
+	private Map<String, Object> retrieveNestedMapping(Map<String, Object> inputObjectMap,
+			Map<String, Object> existingObjectMap) {
 
-        if (expectedJsonObjMap.get(key) instanceof String) {
+		JSONObject interObj = new JSONObject();
 
-          finalObj.put(key, actualJsonObj.get(key));
+		inputObjectMap.forEach((inputKey, inputValue) -> existingObjectMap.forEach((existingKey, existingValue) -> {
+			if (inputKey.equals(existingKey)) {
 
-        } else if (expectedJsonObjMap.get(key) instanceof Map<?, ?>) {
+				if (inputValue instanceof String) {
 
-          finalObj.put(key, retrieveNestedMapping((Map<String, Object>) expectedJsonObjMap.get(key),
-              (Map<String, Object>) actualJsonObjMap.get(key)));
+					interObj.put(inputKey, existingObjectMap.get(inputKey));
 
-        }
-      }
-    });
-    return finalObj.toMap();
-  }
+				} else if (inputValue instanceof Map<?, ?>) {
 
-  @SuppressWarnings("unchecked")
-  private Map<String, Object> retrieveNestedMapping(Map<String, Object> inputObjectMap,
-      Map<String, Object> existingObjectMap) {
+					interObj.put(inputKey, retrieveNestedMapping((Map<String, Object>) inputObjectMap.get(inputKey),
+							(Map<String, Object>) existingObjectMap.get(inputKey)));
 
-    JSONObject interObj = new JSONObject();
+				} else if (inputValue instanceof List<?>) {
 
-    inputObjectMap.forEach(
-        (inputKey, inputValue) -> existingObjectMap.forEach((existingKey, existingValue) -> {
-          if (inputKey.equals(existingKey)) {
+					List<?> interObjList = (List<?>) existingValue;
+					List<Map<?, ?>> constructedInterObjList = new ArrayList<>();
+					List<?> inputListSingleObj = new ArrayList<>();
 
-            if (inputValue instanceof String) {
+					if (inputObjectMap.get(inputKey) instanceof List<?>) {
+						inputListSingleObj = (List<?>) inputValue;
+					}
 
-              interObj.put(inputKey, existingObjectMap.get(inputKey));
+					for (Object interValueObj : interObjList) {
+						constructedInterObjList.add(retrieveNestedMapping(
+								(Map<String, Object>) inputListSingleObj.get(0), (Map<String, Object>) interValueObj));
+					}
 
-            } else if (inputValue instanceof Map<?, ?>) {
+					interObj.put(inputKey, constructedInterObjList);
 
-              interObj.put(inputKey,
-                  retrieveNestedMapping((Map<String, Object>) inputObjectMap.get(inputKey),
-                      (Map<String, Object>) existingObjectMap.get(inputKey)));
+				}
+			}
+		}));
 
-            } else if (inputValue instanceof List<?>) {
-
-              List<?> interObjList = (List<?>) existingValue;
-              List<Map<?, ?>> constructedInterObjList = new ArrayList<>();
-              List<?> inputListSingleObj = new ArrayList<>();
-
-              if (inputObjectMap.get(inputKey) instanceof List<?>) {
-                inputListSingleObj = (List<?>) inputValue;
-              }
-
-              for (Object interValueObj : interObjList) {
-                constructedInterObjList
-                    .add(retrieveNestedMapping((Map<String, Object>) inputListSingleObj.get(0),
-                        (Map<String, Object>) interValueObj));
-              }
-
-              interObj.put(inputKey, constructedInterObjList);
-
-            }
-          }
-        }));
-
-    return interObj.toMap();
-  }
+		return interObj.toMap();
+	}
 
 }
